0e3d53809a065f1c227e291ad719e6f56157aa07
samia-kherbouche <kherbouchesamia28@gmail.com>
2023-01-21 18:16:31 +0100

    fix{table-pagination}

--- mission4us/src/pages/Jobs/index.jsx
+++ mission4us/src/pages/Jobs/index.jsx
@@ -6,27 +6,37 @@ import Head from "../../components/Head";
 import Body from "../../components/Body";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
+// import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell,{ tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
 import { useNavigate } from 'react-router-dom';
+import { CalendarMonthOutlined } from "@mui/icons-material";
+import EditIcon from "@material-ui/icons/Edit";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const useButtonStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+    flexDirection: 'row',
+    
+    
   },
 }));
 
@@ -52,33 +62,115 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(ID, Description, Nom, Prenom, Secteur) {
-  return {ID,Description,Nom,Prenom,Secteur};
-}
 
-const rows = [
-  createData(1, 'front Enginner', 'Kherbouche','Samia' , 'Prive'),
-  createData(2, 'fullstack Enginner', 'Bekka','Reda' , 'Prive'),
+
+// const rows = [
+//   createData(1, 'front Enginner', 'Kherbouche','Samia' , 'Prive'),
+//   createData(2, 'fullstack Enginner', 'Bekka','Reda' , 'Prive'),
   
+// ];
+
+
+// const useStyles = makeStyles({
+//   table: {
+//   marginTop:100,
+//   minWidth: 900,
+//   },
+// });
+const columns = [
+  { id: "nom", label: "Nom", minWidth: 100},
+  { id: "prenom", label: "Prénom", minWidth: 100 },
+  {
+    id: "description",
+    label: "Description",
+    minWidth: 100,
+    align: "left",
+    format: (value) => value.toLocaleString("en-US")
+  },
+  {
+    id: "secteur",
+    label: "Secteur",
+    minWidth: 100,
+    align: "left",
+    format: (value) => value.toLocaleString("en-US")
+  },
+  {
+    id: "actions",
+    label: "Actions",
+    format: (value) => value.toLocaleString("en-US"),
+    // render: rowData => {
+    //   return (
+    //     <div>
+    //       <IconButton
+    //         aria-label="edit"
+         
+    //       >
+    //         <EditIcon />
+    //         {/* {loading===rowData.id && (
+    //           <CircularProgress
+    //             size={38}
+    //             className={classes.fabProgress}
+    //           />
+    //         )} */}
+    //       </IconButton>
+    //       <IconButton aria-label="delete">
+    //         <DeleteIcon />
+    //       </IconButton>
+    //     </div>
+    //   );
+    // }
+    
+  },
+  // {
+  //   id: "density",
+  //   label: "Density",
+  //   minWidth: 170,
+  //   align: "right",
+  //   format: (value) => value.toFixed(2)
+  // }
 ];
 
 
-const useStyles = makeStyles({
-  table: {
-  marginTop:100,
-  minWidth: 900,
-  },
-});
+function createData(nom, prenom, description, secteur) {
+  
+  return { nom, prenom, description, secteur };
+}
 
+const rows = [
+  createData('Kherbouche','Samia','front-end Enginner' , 'Prive'),
+  createData('Bekka','Reda','fullstack Enginner' , 'Prive'),
+  createData('Amariche','Zineddine','fullstack Enginner' , 'Prive'),
+  createData('Kherbouche','Samia','front-end Enginner' , 'Prive'),
+  createData('Bekka','Reda','fullstack Enginner' , 'Prive'),
+  createData('Amariche','Zineddine','fullstack Enginner' , 'Prive'),
+  createData('Kherbouche','Samia','front-end Enginner' , 'Prive'),
+  createData('Bekka','Reda','fullstack Enginner' , 'Prive'),
+  createData('Amariche','Zineddine','fullstack Enginner' , 'Prive'),
+  createData('Kherbouche','Samia','front-end Enginner' , 'Prive'),
+  createData('Bekka','Reda','fullstack Enginner' , 'Prive'),
+  createData('Amariche','Zineddine','fullstack Enginner' , 'Prive'),
+
+];
 
 
 
 const Jobs = () => {
   const theme = useTheme();
-  const classes = useStyles();
+  // const classes = useStyles();
     const buttonStyle=useButtonStyles()
 
      let navigate=useNavigate()
+    const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   return (
     <Box className="dashboard">
       <Head title='Jobs' />
@@ -88,7 +180,7 @@ const Jobs = () => {
         <Button variant="contained" color="primary" >ADD User</Button> 
         </div> */}
          
-            <TableContainer component={Paper}>
+            {/* <TableContainer component={Paper}>
                 <Table  sx={{ minWidth: 700 }} aria-label="customized table">
                     <TableHead>
                     <TableRow>
@@ -138,10 +230,68 @@ const Jobs = () => {
                   ))}
                     </TableBody>
                 </Table>
-            </TableContainer>
+            </TableContainer> */}
+       <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <StyledTableRow>
+              {columns.map((column) => (
+                <StyledTableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </StyledTableCell>
+              ))}
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <StyledTableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                     <StyledTableCell >{row.nom}</StyledTableCell>
+                        <StyledTableCell>{row.prenom}</StyledTableCell>
+                        <StyledTableCell >{row.description}</StyledTableCell>
+                        <StyledTableCell>{row.secteur}</StyledTableCell>
+                        
+                        <StyledTableCell align="left">
+                        <div className={buttonStyle.root}>
+                        <IconButton
+                          aria-label="edit"
+                          color='primary'
+                          onClick={()=>navigate("Update Job")  }
+                        >
+                          <EditIcon />
+                          
+                        </IconButton>
+                        <IconButton aria-label="delete" color='secondary'>
+                          <DeleteIcon />
+                        </IconButton>
+                          </div>
+                          </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        style={{backgroundColor:'#34b782'}}
+      />
+    </Paper>
        
-        
-      </Body>
+      </Body> 
     </Box>
   );
 };
