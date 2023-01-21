872e9e7a41af5e118c462d5ff5c320e90fedbdcc
samia-kherbouche <kherbouchesamia28@gmail.com>
2023-01-21 18:21:19 +0100

    fix{table-pagination}

--- mission4us/src/pages/Jobs/index.jsx
+++ mission4us/src/pages/Jobs/index.jsx
@@ -6,17 +6,8 @@ import Head from "../../components/Head";
 import Body from "../../components/Body";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { styled } from '@mui/material/styles';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-// import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell,{ tableCellClasses } from '@mui/material/TableCell';
@@ -26,7 +17,6 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 import { useNavigate } from 'react-router-dom';
-import { CalendarMonthOutlined } from "@mui/icons-material";
 import EditIcon from "@material-ui/icons/Edit";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -62,21 +52,6 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-
-
-// const rows = [
-//   createData(1, 'front Enginner', 'Kherbouche','Samia' , 'Prive'),
-//   createData(2, 'fullstack Enginner', 'Bekka','Reda' , 'Prive'),
-  
-// ];
-
-
-// const useStyles = makeStyles({
-//   table: {
-//   marginTop:100,
-//   minWidth: 900,
-//   },
-// });
 const columns = [
   { id: "nom", label: "Nom", minWidth: 100},
   { id: "prenom", label: "Prénom", minWidth: 100 },
@@ -97,37 +72,8 @@ const columns = [
   {
     id: "actions",
     label: "Actions",
-    format: (value) => value.toLocaleString("en-US"),
-    // render: rowData => {
-    //   return (
-    //     <div>
-    //       <IconButton
-    //         aria-label="edit"
-         
-    //       >
-    //         <EditIcon />
-    //         {/* {loading===rowData.id && (
-    //           <CircularProgress
-    //             size={38}
-    //             className={classes.fabProgress}
-    //           />
-    //         )} */}
-    //       </IconButton>
-    //       <IconButton aria-label="delete">
-    //         <DeleteIcon />
-    //       </IconButton>
-    //     </div>
-    //   );
-    // }
-    
+    format: (value) => value.toLocaleString("en-US"),  
   },
-  // {
-  //   id: "density",
-  //   label: "Density",
-  //   minWidth: 170,
-  //   align: "right",
-  //   format: (value) => value.toFixed(2)
-  // }
 ];
 
 
@@ -175,63 +121,7 @@ const Jobs = () => {
     <Box className="dashboard">
       <Head title='Jobs' />
       <Body>
-      
-        {/* <div className={buttonStyle.root}> 
-        <Button variant="contained" color="primary" >ADD User</Button> 
-        </div> */}
-         
-            {/* <TableContainer component={Paper}>
-                <Table  sx={{ minWidth: 700 }} aria-label="customized table">
-                    <TableHead>
-                    <TableRow>
-                        <StyledTableCell>ID</StyledTableCell>
-                        <StyledTableCell align="center">Description</StyledTableCell>
-                        <StyledTableCell align="center">Nom</StyledTableCell>
-                        <StyledTableCell align="center">Prenom</StyledTableCell>
-                        <StyledTableCell align="center">Secteur</StyledTableCell>
-                        <StyledTableCell align="center">Actions</StyledTableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow key={row.ID}>
-                        <StyledTableCell component="th" scope="row">
-                            {row.ID}
-                        </StyledTableCell>
-                        <StyledTableCell align="center">{row.Description}</StyledTableCell>
-                        <StyledTableCell align="center">{row.Nom}</StyledTableCell>
-                        <StyledTableCell align="center">{row.Prenom}</StyledTableCell>
-                        <StyledTableCell align="center">{row.Secteur}</StyledTableCell>
-                        <StyledTableCell align="center">
-                        <div className={buttonStyle.root}>
-                            <ButtonGroup variant="contained"  aria-label="contained primary button group" >
-                              <Button color="default" 
-                              style={{marginRight:"5px"}}
-                              onClick={()=>navigate("Add Job")  }
-                              >
-                                Ajouter
-                              </Button>
-                              <Button 
-                              color="primary" 
-                              style={{marginRight:"5px"}}
-                              onClick={()=>navigate("Update Job")  }
-                               >Modifier</Button>
-                                <Button 
-                                color="secondary" 
-                               
-                                
-                                >Supprimer</Button>
-                                
-                               
-                            </ButtonGroup>
-                          </div>
-                        </StyledTableCell>
-                        </StyledTableRow>
-                  ))}
-                    </TableBody>
-                </Table>
-            </TableContainer> */}
-       <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
