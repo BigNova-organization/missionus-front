954b971fd3898cb1fbcd6755b69f39ec2bc19025
samia-kherbouche <kherbouchesamia28@gmail.com>
2023-01-24 14:57:48 +0100

    feat(mission-add-right-sidebar)

---
+++ mission4us/src/components/Drawer/Drawer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Drawer.module.css";
+import { changeAnchor } from "../../helpers/change-anchor";
+
+const Drawer = (props) => {
+  const { open, anchor, onClose } = props;
+  const {
+    drawer,
+    animate,
+    hidden,
+    overlay,
+    overlayOpen,
+    overlayHidden,
+    header
+  } = styles;
+
+  return (
+    <>
+      <div
+        className={`${overlay} ${!open && overlayHidden} ${
+          open && overlayOpen
+        }`}
+        onClick={onClose}
+        aria-hidden="true"
+      />
+      <div
+        tabIndex="-1"
+        className={`${drawer} ${open && animate} ${
+          !open && hidden
+        } ${changeAnchor(anchor,styles)}`}
+      >
+        <div className={header} />
+      </div>
+    </>
+  );
+};
+
+Drawer.propTypes = {
+  open: PropTypes.bool.isRequired,
+  anchor: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired
+};
+export default Drawer
--- mission4us/src/pages/missions/index.jsx
+++ mission4us/src/pages/missions/index.jsx
@@ -1,16 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState,useCallback } from "react";
 import { Box, useTheme } from "@mui/material";
 
 import "./styles.css";
 import Head from "../../components/Head";
-
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Drawer from "../../components/Drawer/Drawer.jsx";
 
 
 const Missions = () => {
   const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = useCallback(() => setOpen(true), []);
+
+  const handleClose = useCallback(() => setOpen(false), []);
   return (
     <Box className="dashboard">
-      <Head title='Missions' />
+      <Box
+      sx={{
+        padding: "10px",
+        color: theme.palette.grey[100],
+        fontSize: "22px",
+        backgroundColor: theme.palette.background.default ,
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'space-between'
+
+      }}
+    >
+      <div><p>Missions</p></div>
+      <div>
+      <Button variant="contained" 
+      endIcon={<AddIcon />} 
+      size='medium' 
+      style={{backgroundColor:'#237a57'}}
+      onClick={handleOpen}
+      >
+        Ajouter
+      </Button>
+      <Drawer anchor="right" open={open} onClose={handleClose} />
+      </div>
+    </Box>
     </Box>
   );
 };
