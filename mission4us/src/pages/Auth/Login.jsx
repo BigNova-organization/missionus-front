a21a163a4c14a0d7cce212f62e531b5845a4d604
zinou11-y <insigned11@gmail.com>
2023-03-21 09:04:13 +0000

    check branch

--- mission4us/src/pages/Auth/Login.jsx
+++ mission4us/src/pages/Auth/Login.jsx
@@ -1,36 +1,36 @@
 import {useNavigate} from "react-router-dom"
 import React from "react";
 import Keycloak from 'keycloak-js';
-let initOptions = {
-    url: 'https://auth.mission4us.com/auth',
-    realm: 'local_tests',
-    clientId: 'myclient',
-    onLoad: 'login-required',
-    KeycloakResponseType: 'code'
-}
-const keycloak =new Keycloak(initOptions);
-keycloak.init({ onLoad: initOptions.onLoad, KeycloakResponseType: 'code' }).then((auth) => {
-    if (!auth) {
-        window.location.reload();
-    } else {
-        console.info("Authenticated");
-    }
-    setTimeout(() => {
-        keycloak.updateToken(70).then((refreshed) => {
-            if (refreshed) {
-                console.debug('Token refreshed' + refreshed);
-            } else {
-                console.warn('Token not refreshed, valid for '
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-            }
-        }).catch((error) => {
-            console.error(error,'Failed to refresh token');
-        });
+// let initOptions = {
+//     url: 'https://auth.mission4us.com/auth',
+//     realm: 'local_tests',
+//     clientId: 'myclient',
+//     onLoad: 'login-required',
+//     KeycloakResponseType: 'code'
+// }
+// const keycloak =new Keycloak(initOptions);
+// keycloak.init({ onLoad: initOptions.onLoad, KeycloakResponseType: 'code' }).then((auth) => {
+//     if (!auth) {
+//         window.location.reload();
+//     } else {
+//         console.info("Authenticated");
+//     }
+//     setTimeout(() => {
+//         keycloak.updateToken(70).then((refreshed) => {
+//             if (refreshed) {
+//                 console.debug('Token refreshed' + refreshed);
+//             } else {
+//                 console.warn('Token not refreshed, valid for '
+//                     + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+//             }
+//         }).catch((error) => {
+//             console.error(error,'Failed to refresh token');
+//         });
 
-    }, 60000)
-}).catch((error) => {
-    console.error(error,"Authenticated Failed");
-});
+//     }, 60000)
+// }).catch((error) => {
+//     console.error(error,"Authenticated Failed");
+// });
 const Login = () => {
 	// const navigate = useNavigate()
 
