d0deb06b7f665f1497317dabe74abc7bd61ee372
samia-kherbouche <kherbouchesamia28@gmail.com>
2023-03-22 10:27:58 +0100

    keycloak-auth

--- mission4us/src/pages/Auth/Login.jsx
+++ mission4us/src/pages/Auth/Login.jsx
@@ -1,20 +1,30 @@
 import {useNavigate} from "react-router-dom"
-import React from "react";
+import React, { useEffect } from "react";
+import { Box } from "@mui/system";
 import Keycloak from 'keycloak-js';
+// import axios from 'axios';
+
+
 let initOptions = {
     url: 'https://auth.mission4us.com/auth',
     realm: 'local_tests',
     clientId: 'myclient',
     onLoad: 'login-required',
-    KeycloakResponseType: 'code'
+    
+    // KeycloakResponseType: 'code'
 }
-const keycloak =new Keycloak(initOptions);
+const keycloak = new Keycloak(initOptions);
+
 keycloak.init({ onLoad: initOptions.onLoad, KeycloakResponseType: 'code' }).then((auth) => {
+    console.log(keycloak,'key')
     if (!auth) {
         window.location.reload();
     } else {
         console.info("Authenticated");
     }
+    localStorage.setItem("bearer-token", keycloak.token);
+    localStorage.setItem("refresh-token", keycloak.refreshToken);
+    console.log(keycloak.token);
     setTimeout(() => {
         keycloak.updateToken(70).then((refreshed) => {
             if (refreshed) {
@@ -37,16 +47,17 @@ const Login = () => {
 	//ADMIN
 	//USER
 
-	  const login = () => {
-	  	localStorage.setItem("user", JSON.stringify({role: "USER"}))
-	  	navigate("/dashboard")
-	  }
+	//   const login = () => {
+	//   	localStorage.setItem("user", JSON.stringify({role: "USER"}))
+	//   	navigate("/dashboard")
+	//   }
+
 
 	return (
     <Box sx={{display:"flex" , justifyContent:'center' , alignItems:'center' , flexDirection:"column" , flex:1,backgroundColor:"#990" ,flexGrow:1,padding:20}}  >
     <h2>Welcome to login page! </h2>
     <p>Please loging to continue</p>
-    <button onClick={login}> Login</button>
+    {/* <button onClick={login}> Login</button> */}
   </Box>
         // <>
         // </>
