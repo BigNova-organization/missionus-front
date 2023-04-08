573fb1a7b920c989343d0eca31dcd9589d178098
samia-kherbouche <kherbouchesamia28@gmail.com>
2023-04-08 15:29:48 +0100

    feat(visu-cv):add_share_social_media_buttons

--- mission4us/src/components/Head.jsx
+++ mission4us/src/components/Head.jsx
@@ -1,31 +1,92 @@
-import React from "react";
-import { Box, useTheme } from "@mui/material";
-import { ChevronLeft } from "@mui/icons-material";
+  import React from "react";
+  import { Box, Tooltip, useTheme } from "@mui/material";
+  import { ChevronLeft } from "@mui/icons-material";
+  import { FacebookShareButton,TwitterShareButton,LinkedinShareButton, FacebookIcon,TwitterIcon,LinkedinIcon} from 'react-share';
+  import { Stack } from "@mui/system";
+import Space from "./outils/Space";
+import FlexBetween from "./FlexBetween";
+  const Head = ({ title, retur,btn }) => {
+    const theme = useTheme();
+    const handleShareResume = () => {
+      // Use the 'navigator.share()' API to share the resume
+      if (navigator.share) {
+        navigator.share({
+          title: 'My Resume',
+          text: 'Check out my resume!',
+          url: 'https://example.com/my-resume.pdf',
+        })
+          .then(() => console.log('Resume shared successfully'))
+          .catch(error => console.error('Error sharing resume:', error));
+      } else {
+        console.log('Web Share API not supported on this browser');
+      }
+    }
 
-const Head = ({ title, retur }) => {
-  const theme = useTheme();
+    return (
+      <Box
+        sx={{
+          padding: "25px",
+          color: theme.palette.primary.light,
+          fontSize: "22px",
+          backgroundColor: theme.palette.background.default,
+          display: "flex",
+          alignItems: "center",
+          cursor: "pointer",
+          "&:hover": {
+            textDecoration: retur ? "underline" : "none",
+          },
+        }}
+      >
+       
+        {retur ? (
+          <ChevronLeft sx={{ color: theme.palette.primary.light }} />
+        ) : null}
+       
+        {title}
+        {/* <Space space={"20px"} /> */}
+      
+  {btn?(
+    <div style={{paddingLeft:20}}>
+    <Tooltip title="Partager sur Facebook">
+    <FacebookShareButton
+    url={'https://example.com/my-resume.pdf'}
+    quote={'Check out my resume!'}
+    hashtag={'#resume'}
+    onClick={handleShareResume}
+  >
+    <FacebookIcon size={32} round />
+  </FacebookShareButton>
+  </Tooltip>
 
-  return (
-    <Box
-      sx={{
-        padding: "25px",
-        color: theme.palette.primary.light,
-        fontSize: "22px",
-        backgroundColor: theme.palette.background.default,
-        display: "felx",
-        alignItems: "center",
-        cursor: "pointer",
-        "&:hover": {
-          textDecoration: retur ? "underline" : "none",
-        },
-      }}
-    >
-      {retur ? (
-        <ChevronLeft sx={{ color: theme.palette.primary.light }} />
-      ) : null}
-      {title}
-    </Box>
-  );
-};
+  <Tooltip title="Partager sur Twitter">
+  <TwitterShareButton
+    url={'https://example.com/my-resume.pdf'}
+    title={'My Resume'}
+    hashtags={['resume']}
+    onClick={handleShareResume}
+  >
+    <TwitterIcon size={32} round />
+  </TwitterShareButton>
+  </Tooltip>
 
-export default Head;
+  <Tooltip title="Partager sur Linkedin">
+  <LinkedinShareButton
+    url={'https://example.com/my-resume.pdf'}
+    title={'My Resume'}
+    summary={'Check out my resume!'}
+    source={'My Website'}
+    onClick={handleShareResume}
+  >
+    <LinkedinIcon size={32} round />
+  </LinkedinShareButton>
+
+  </Tooltip>
+  </div>
+      
+  ):null}
+     
+      </Box>
+    );
+  };
+
+  export default Head;
--- mission4us/src/pages/VisualiserCv/index.jsx
+++ mission4us/src/pages/VisualiserCv/index.jsx
@@ -9,6 +9,9 @@ import Formation from "./components/formations";
 import Competence from "./components/competence";
 import Hobies from "./components/hobie";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { FacebookShareButton,TwitterShareButton,LinkedinShareButton, FacebookIcon,TwitterIcon,LinkedinIcon} from 'react-share';
+// import  ShareButtons from 'react-share';
+// import generateShareIcon from 'react-share';
 
 const VisualiserCv = ( props) => {
 
@@ -18,13 +21,75 @@ const VisualiserCv = ( props) => {
 
   const theme = useTheme();
 
+  // const FacebookShareButton = ShareButtons.FacebookShareButton;
+  // const FacebookIcon = generateShareIcon('facebook');
+  
+  // const TwitterShareButton = ShareButtons.TwitterShareButton;
+  // const TwitterIcon = generateShareIcon('twitter');
+  
+  // const LinkedinShareButton = ShareButtons.LinkedInShareButton;
+  // const LinkedinIcon = generateShareIcon('linkedin');
+
+  // const handleShareResume = () => {
+  //   // Use the 'navigator.share()' API to share the resume
+  //   if (navigator.share) {
+  //     navigator.share({
+  //       title: 'My Resume',
+  //       text: 'Check out my resume!',
+  //       url: 'https://example.com/my-resume.pdf',
+  //     })
+  //       .then(() => console.log('Resume shared successfully'))
+  //       .catch(error => console.error('Error sharing resume:', error));
+  //   } else {
+  //     console.log('Web Share API not supported on this browser');
+  //   }
+  // }
  
 
   
   return (
     <>
-      <Head title="Visualiser Curriculum Vitae" retur/>
+    <Head title="Visualiser Curriculum Vitae" retur btn/>
+
+    {/* <FacebookShareButton
+    url={'https://example.com/my-resume.pdf'}
+    quote={'Check out my resume!'}
+    hashtag={'#resume'}
+    onClick={handleShareResume}
+  >
+    <FacebookIcon size={32} round />
+  </FacebookShareButton>
+
+  <TwitterShareButton
+    url={'https://example.com/my-resume.pdf'}
+    title={'My Resume'}
+    hashtags={['resume']}
+    onClick={handleShareResume}
+  >
+    <TwitterIcon size={32} round />
+  </TwitterShareButton>
+
+  <LinkedinShareButton
+    url={'https://example.com/my-resume.pdf'}
+    title={'My Resume'}
+    summary={'Check out my resume!'}
+    source={'My Website'}
+    onClick={handleShareResume}
+  >
+    <LinkedinIcon size={32} round />
+  </LinkedinShareButton>
+
+       */}
       <Body>
+        {/* <Stack>
+        <PrimaryText
+          fontWeight={"600"}
+          fontSize={"35px"}
+          text={" Créez votre cv"}
+          color={theme.palette.secondary.light}
+          cursor
+        />
+        </Stack> */}
         <Stack>
           <Presentation state={state} />
           <Space space={20} />
