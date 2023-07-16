98e7bea019778ef23ee8e010013634b415b4ac89
zinou11-y <insigned12@gmail.com>
2023-07-16 16:04:22 +0100

    - update multi select fonctionality

--- src/components/outils/multipleSelect.jsx
+++ src/components/outils/multipleSelect.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import ListItemText from "@mui/material/ListItemText";
@@ -8,10 +7,10 @@ import Select from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
 import { FormHelperText, useTheme } from "@mui/material";
 import { makeStyles } from "@material-ui/core/styles";
-import { useDispatch, useSelector } from "react-redux";
+import {  useSelector } from "react-redux";
+import { PrimaryText } from "../utils/typography";
+import Space from "./Space";
 import { useEffect } from "react";
-import { fetchJobs } from "../../Redux/jobs/slice";
-import { createEmploi } from "../../Redux/createCv/slice";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -65,7 +64,6 @@ const names = [
 
 export default function MultipleSelectCheckmarks({
   label,
-  data,
   handleOpen,
   selectionTitle,
   error,
@@ -76,51 +74,45 @@ export default function MultipleSelectCheckmarks({
   disabled,
   multiple,
   renderValue,
-  setFieldValue
+  setFieldValue,
+  status,
+  data,name,personNames
+
 }) {
-  const [personName, setPersonName] = React.useState([]);
+  const [personName, setPersonName] = React.useState(personNames);
   const theme = useTheme();
   const { mode } = useSelector((state) => state.global);
-  const { jobs ,status} = useSelector((state) => state.jobs);
-  const { EmploiArr } = useSelector((state) => state.cvs);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchJobs());
-  }, [dispatch]);
-
 
 
 const handleChange = (event) => {
   const { value } = event.target;
   setPersonName(value);
-  setFieldValue("jobs", value.join(", "));
+  setFieldValue(name, value.join(", "));
 };
-
-
-// const handleChange = (event) => {
-//   const { value } = event.target;
-//   const selectedJobs = value.map((jobName) => {
-//     const job = jobs.find((j) => j.name === jobName);
-//     return { id: job.id, label: jobName };
-//   });
-//   setPersonName(selectedJobs);
-//   setFieldValue("jobs", selectedJobs.map((job) => job.label).join(", "));
-// };
-
-
-
-// console.log('jobs', jobs)
+ 
 
   const classes = useStyles();
   return (
-    <div>
-      <FormControl sx={{ m: 1, width: 300 }}>
+      <FormControl sx={{  width: "100%" }}>
         {/* <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel> */}
-        {jobs?.length == 0 || status == 'loading' ? (
+        {status?.length == 0 || status == 'loading' ? (
           <div>chargement ...</div>
         ) : (
+
+          <>
+          <Space space={"20px"} />
+          
+          <PrimaryText
+                    fontWeight={"600"}
+                    fontSize={"20px"}
+                    text={label}
+                    color={
+                      theme.palette.secondary.light
+                    }
+                  />
+          
+          <Space space={"20px"} />
+          
           <Select
             labelId="demo-multiple-checkbox-label"
             id="demo-multiple-checkbox"
@@ -144,131 +136,27 @@ const handleChange = (event) => {
                 icon: mode == "light" ? classes.icon : classes.iconDark,
               },
             }}
+            InputLabelProps={{
+              style: {
+                color:theme.palette.secondary.dark,
+              },
+            }}
           >
-            {jobs?.map((i) => (
+            {data?.map((i) => (
               <MenuItem key={i.name} value={i.name}>
                 <Checkbox checked={personName.indexOf(i.name) > -1} />
+
                 <ListItemText primary={i.name} />
               </MenuItem>
             ))}
           </Select>
+          </>
         )}
         <FormHelperText sx={{ color: theme.palette.error.main, pl: 3 }}>
           {helperText}
         </FormHelperText>
       </FormControl>
-    </div>
   );
 }
 
-
-  // const updatedEmploiArr = names
-  //   .filter((name) => value.includes(name))
-  //   .map((name) => ({
-  //     key: EmploiArr.length + Math.floor(Math.random() * 9) + 1,
-  //     label: [name],
-  //   }));
-
-  // dispatch(createEmploi(updatedEmploiArr));
-
-//   const handleChange = (event) => {
-//     const {
-//       target: { value },
-//     } = event;
-//     setPersonName(
-//       // On autofill we get a stringified value.
-//       typeof value === "string" ? value.split(",") : value
-//     );
-//     // dispatch(
-//     //   createEmploi(personName)
-//     // );
-// console.log('value', value)
-//     dispatch(
-//       createEmploi([
-//         ...EmploiArr,
-//         { key: EmploiArr.length+Math.floor(Math.random() * (9 - 1 + 1) + 1) + 1 + 1, label: value },
-        
-//       ])
-//     );
-//     const jobsd = personName.join(", ");
-//     setFieldValue("jobs",   jobsd);
-
-//   };
-
-// const handleChange = (event) => {
-//   const { target: { value } } = event;
-
-//   // Split the value into an array of individual jobs
-//   const jobs = typeof value === "string" ? value.split(",") : value;
-
-//   // Create a new emploi object with a unique key and the label set to each job
-//   const emploiObject = {
-//     key: EmploiArr.length + Math.floor(Math.random() * 9) + 1,
-//     label: jobs,
-//   };
-
-//   // Update the emploi array by adding the new emploi object
-//   const updatedEmploiArr = [...EmploiArr, emploiObject];
-
-//   console.log('updatedEmploiArr', updatedEmploiArr);
-
-//   // Update the state variables
-//   setPersonName(jobs);
-//   dispatch(createEmploi(updatedEmploiArr));
-//   setFieldValue("jobs", jobs.join(", "));
-// };
-
-// const handleChange = (event) => {
-//   const { target: { value } } = event;
-
-//   // Split the value into an array of individual jobs
-//   const jobs = typeof value === "string" ? value.split(",") : value;
-
-//   // Create an array of new emploi objects for each job
-//   const emploiObjects = jobs.map((job) => ({
-//     key: EmploiArr.length + Math.floor(Math.random() * 9) + 1,
-//     label: [job],
-//   }));
-
-//   // Update the emploi array by adding the new emploi objects
-//   const updatedEmploiArr = [...EmploiArr, ...emploiObjects];
-
-//   console.log('updatedEmploiArr', updatedEmploiArr);
-
-//   // Update the state variables
-//   setPersonName(jobs);
-//   dispatch(createEmploi(updatedEmploiArr));
-//   setFieldValue("jobs", jobs.join(", "));
-// };
-
-// const handleChange = (event) => {
-//   const { target: { value, checked } } = event;
-
-//   // Split the value into an array of individual jobs
-//   const jobs = typeof value === "string" ? value.split(",") : value;
-
-//   // Check if the checkbox was checked or unchecked
-//   if (checked) {
-//     // Create new emploi objects for each job
-//     const emploiObjects = jobs.map((job) => ({
-//       key: EmploiArr.length + Math.floor(Math.random() * 9) + 1,
-//       label: [job],
-//     }));
-
-//     // Update the emploi array by adding the new emploi objects
-//     const updatedEmploiArr = [...EmploiArr, ...emploiObjects];
-
-//     // Update the state variables
-//     setPersonName(jobs);
-//     dispatch(createEmploi(updatedEmploiArr));
-//     setFieldValue("jobs", jobs.join(", "));
-//   } else {
-//     // Filter out the unchecked jobs from the emploi array
-//     const updatedEmploiArr = EmploiArr.filter((emploi) => !jobs.includes(emploi.label[0]));
-
-//     // Update the state variables
-//     setPersonName(jobs);
-//     dispatch(createEmploi(updatedEmploiArr));
-//     setFieldValue("jobs", jobs.join(", "));
-//   }
-// };
+ 
